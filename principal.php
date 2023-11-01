88bdf63505e7b82e5ed4b79315a114b70634a70d
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-10-31 21:12:39 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -1,9 +1,24 @@
 <?php
 
-session_start();
-
-include('conexao.php');
-
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+
+          <h1>Olá, administrador</h1>
+          <a href="adicionar.php">
+           Adicionar usuário</a><br>
+
+    </center>
+</body>
+</html>
+
