700bd794ac07ffd9c1a11441d45c79c29f4561bf
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-10-31 21:12:10 -0300

    Add files via upload

---
+++ adicionar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+
+<form id="form-add" action="#" method="POST">
+    
+      Nome: <input type = "text" name = "nome"><br>
+      CPF: <input type= "text" name = "cpf"><br>
+      Telefone: <input type = "text" name = "telefone"><br>
+      login: <input type = "text" name = "login"><br>
+      senha: <input type = "password" name = "senha"><br>
+      <input type = "submit" name = "cadastrar" value = "cadastrar">
+
+</form>
+    </center>
+</body>
+</html>
