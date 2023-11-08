2436215e51f04cb20939292457794142208248b8
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-11-07 21:22:21 -0300

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -1,3 +1,51 @@
+<?php
+
+session_start();
+include('conexao.php');
+include('funcoes.php');
+
+$nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+$cpf = isset($_POST['cpf']) ? $_POST['cpf'] : '';
+$telefone = isset($_POST['telefone']) ? $_POST['telefone'] : '';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+
+$selectcpf = "SELECT cpf FROM usuario WHERE cpf = '$cpf'";
+$querycpf = mysqli_query($conexao,$selectcpf);
+$dadocpf = mysqli_fetch_row($querycpf);
+
+$selectlogin = "SELECT login FROM login WHERE login = '$login'";
+$querylogin = mysqli_query($conexao,$selectlogin);
+$dadologin = mysqli_fetch_row($querylogin);
+
+if($nome <> NULL) {
+
+if (($dadocpf == NULL) && ($dadologin == NULL)) {
+    $insertusuario = "INSERT INTO usuario (nome, cpf,telefone) 
+    VALUES ('$nome', '$cpf' , '$telefone')"; 
+    $queryusuario = mysqli_query($conexao,$insertusuario);
+    $senhacriptografada = criptografar($senha);
+    
+    $insertlogin = " INSERT INTO login (cpf,login,senha,nivel) 
+    VALUES
+    ('$cpf','$login','$senhacriptografada' , 3)";
+    $querylogin = mysqli_query($conexao,$insertlogin);
+
+    echo'<script>alert("usuario cadastrado com sucesso");
+    window.location="adicionar.php";
+    </script>';
+
+} else { echo'<script>alert("CPF e/ou login já cadastrados no sistema");
+    window.location="adicionar.php";
+    </script>';
+
+}
+
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +56,7 @@
 </head>
 <body>
     <center>
-
+    <h1>Adicionar usuario</h1>
 <form id="form-add" action="#" method="POST">
     
       Nome: <input type = "text" name = "nome"><br>
